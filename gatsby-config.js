d05f7bc9dee1dab46c43957753ec025954da5cfe
jordanmceachern <jormceachern@gmail.com>
2023-02-04 10:07:45 -0400

    deps update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    siteMetadata: {
-      title: `Brittany McQuinn`,
-      siteUrl: `brittanymcquinn.com`,
-      description: `Brittany McQuinn is a singer songwriter from the province of New Brunswick in the East Coast of Canada.
-        Brittany works actively on solo projects, artist collaborations, and writing/singing lead for her duo with Daniel 
-        James in a band known as Bleum. Visit to read her story, find her music, and get connected with her business contact.`
-    },
-    plugins: [`gatsby-plugin-react-helmet`]
-  }
+  // siteMetadata: {
+  //   title: `Brittany McQuinn`,
+  //   siteUrl: `brittanymcquinn.com`,
+  //   description: `Brittany McQuinn is a singer songwriter from the province of New Brunswick in the East Coast of Canada.
+  //     Brittany works actively on solo projects, artist collaborations, and writing/singing lead for her duo with Daniel
+  //     James in a band known as Bleum. Visit to read her story, find her music, and get connected with her business contact.`
+  // },
+  plugins: [`gatsby-plugin-react-helmet`],
+};
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,74 +1,106 @@
-import React, { Component } from 'react'
-import { Link } from 'gatsby'
-import DarkMode from './darkMode/darkMode'
-import './layout.css'
-import facebook from './facebook.png'
-import instagram from './instagram.png'
-import twitter from './twitter.png'
+import React from 'react';
+import { Link } from 'gatsby';
+import DarkMode from './darkMode/darkMode';
+import './layout.css';
+import facebook from './facebook.png';
+import instagram from './instagram.png';
+import twitter from './twitter.png';
 
-class Layout extends Component {
+const Layout = () => {
+  const toggleHandler = () => {
+    const navhtml = document.getElementsByClassName('mobile');
+    const navArr = Array.from(navhtml);
+    navArr.forEach((element) => element.classList.toggle('hideThis'));
+  };
+  return (
+    <React.Fragment>
+      <nav>
+        <div className='heading'>
+          <div id='hamburger' onClick={toggleHandler}>
+            <hr />
+            <hr />
+            <hr />
+          </div>
+          <Link id='home' to='/'>
+            brittany mcquinn
+          </Link>
+        </div>
+        <Link
+          id='story'
+          className='heading mobile hideThis'
+          to='/story'
+          onClick={toggleHandler}
+        >
+          story
+        </Link>
+        <Link
+          id='lyrics'
+          className='heading mobile hideThis'
+          to='/lyrics'
+          onClick={toggleHandler}
+        >
+          lyrics
+        </Link>
+        <Link
+          id='shows'
+          className='heading mobile hideThis'
+          to='/shows'
+          onClick={toggleHandler}
+        >
+          shows
+        </Link>
+        <Link
+          id='contact'
+          className='heading mobile hideThis'
+          to='/contact'
+          onClick={toggleHandler}
+        >
+          contact
+        </Link>
+        <DarkMode />
+      </nav>
 
-toggleHandler = () => {
-    const navhtml = document.getElementsByClassName("mobile")
-    const navArr = Array.from(navhtml)
-    navArr.forEach(element => element.classList.toggle("hideThis"))
-}
-    
-render(){
-    return (
-        <React.Fragment>
-            <nav>
-                <div className="heading">
-                    <div id="hamburger" onClick={this.toggleHandler}>
-                        <hr />
-                        <hr />
-                        <hr />
-                    </div>
-                    <Link id="home" to="/">brittany mcquinn</Link>
-                </div>
-                <Link id="story" className="heading mobile hideThis" to="/story" onClick={this.toggleHandler}>story</Link>
-                <Link id="lyrics" className="heading mobile hideThis" to="/lyrics" onClick={this.toggleHandler}>lyrics</Link>
-                <Link id="shows" className="heading mobile hideThis" to="/shows" onClick={this.toggleHandler}>shows</Link>
-                <Link id="contact" className="heading mobile hideThis" to="/contact" onClick={this.toggleHandler}>contact</Link>
-                <DarkMode />
-            </nav>
+      <div id='footer'>
+        <div className='links'>
+          <a
+            href='https://www.facebook.com/BrittanyMcQuinn/?epa=SEARCH_BOX&jazoest=265100121975011957685684986995861021111128168708410148113117721019012172867710790103108121851101131144883110104815865100119116106118105525210273791225511648735711680867095805010410180571085179999974551101171091071101055112077119'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img id='fb' src={facebook} alt='link to facebook' />
+          </a>
+        </div>
+        <div className='links'>
+          <a
+            href='https://www.instagram.com/brittanymcquinn/?hl=en'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img src={instagram} alt='link to instagram' />
+          </a>
+        </div>
+        <div className='links'>
+          <a
+            href='https://twitter.com/brittanymcquinn'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img src={twitter} alt='link to twitter' />
+          </a>
+        </div>
+        <div className='links' id='buyDiv'>
+          <a
+            href='https://fanlink.to/BrittanyMcQuinn'
+            target='_blank'
+            rel='noopener noreferrer'
+            id='buy'
+          >
+            <p>stream/ buy</p>
+          </a>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
-            <div id="footer">
-                <div className="links">
-                    <a 
-                    href="https://www.facebook.com/BrittanyMcQuinn/?epa=SEARCH_BOX&jazoest=265100121975011957685684986995861021111128168708410148113117721019012172867710790103108121851101131144883110104815865100119116106118105525210273791225511648735711680867095805010410180571085179999974551101171091071101055112077119" 
-                    target="_blank" 
-                    rel="noopener noreferrer">
-                        <img id="fb" src={facebook} alt="link to facebook"/>
-                    </a>
-                </div>
-                <div className="links">
-                    <a 
-                    href="https://www.instagram.com/brittanymcquinn/?hl=en" 
-                    target="_blank" 
-                    rel="noopener noreferrer">
-                        <img src={instagram} alt="link to instagram"/>
-                    </a>
-                </div>
-                <div className="links">
-                    <a 
-                    href="https://twitter.com/brittanymcquinn" 
-                    target="_blank" rel="noopener noreferrer">
-                        <img src={twitter} alt="link to twitter"/>
-                    </a>
-                </div>
-                <div className="links" id="buyDiv">
-                    <a 
-                    href="https://fanlink.to/BrittanyMcQuinn" 
-                    target="_blank" rel="noopener noreferrer"
-                    id="buy">
-                        <p>stream/ buy</p>
-                    </a>
-                </div>
-            </div>
-        </React.Fragment>
-        )
-    }
-}
-
-export default Layout
+export default Layout;
