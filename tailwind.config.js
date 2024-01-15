6845e9f4769806f8d015d3eac1c84df5f78e6ac6
jordanmceachern <jormceachern@gmail.com>
2024-01-15 10:55:08 -0400

    add config files + rm docs folder

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const withMT = require('@material-tailwind/react/utils/withMT');
+
+module.exports = withMT({
+  content: ['./src/**/*.{html,js,ts,tsx}'],
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       yellow: '#facc15',
+  //       orange: '#fb923c',
+  //       green: '#166534',
+  //       darkblue: '#2563eb',
+  //       blue: '#60a5fa',
+  //       brown: '#964B00',
+  //       red: '#ef4444',
+  //       slate: '#333',
+  //     },
+  //   },
+  // },
+  plugins: [],
+});
