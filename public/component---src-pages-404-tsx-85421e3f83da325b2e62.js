c775935e7f86c03c3ec1c2235632092a5401bdd8
Jordan McEachern <jmceachern@l743y6fjkw.cvent.net>
2024-02-24 13:59:04 -0400

    setup new email subscription using formspree

---
+++ public/component---src-pages-404-tsx-85421e3f83da325b2e62.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[453],{7187:function(e,t,a){a.r(t);var r=a(758),l=a(8057);t.default=e=>{let{location:t}=e;return r.createElement(l.A,{currentPathname:t.pathname},r.createElement("h1",{className:"mx-auto my-24"},r.createElement("p",{className:"text-slate dark:text-white bg-white/60 dark:bg-slate/50 text-center p-4 rounded"},"404: Page Not Found")))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-85421e3f83da325b2e62.js.map
