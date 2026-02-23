73e6b70b3d4227a0b6a267d2c0a6eca9e82af3ef
jordanmceachern <jordan.mceachern@gmail.com>
2026-02-23 14:01:59 -0400

    Updates

---
+++ component---src-pages-index-tsx-5418d3f9cf88d76d2061.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[245],{6439:function(e,t,a){a.r(t);var r=a(758),l=a(8057);t.default=e=>{let{location:t}=e;return r.createElement(l.A,{currentPathname:t.pathname})}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-5418d3f9cf88d76d2061.js.map
